79d1c8bc1bf0f44f79927e55066582c20355e23d
Om Prakash <omprakashdevmail@gmail.com>
2025-10-06 15:57:31 +0530

    Created static organization profile

--- src/app/features/settings/sub-modules/organization-profile/organization-profile.ts
+++ src/app/features/settings/sub-modules/organization-profile/organization-profile.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Card } from "../../../../shared/components/ui/card/card";
+import { Button } from "../../../../shared/components/ui/button/button";
+import { LucideAngularModule } from "lucide-angular";
+import { AppIcons } from '../../../../../assets/icons/icons';
 
 @Component({
   selector: 'app-organization-profile',
-  imports: [],
+  imports: [Card, Button, LucideAngularModule],
   templateUrl: './organization-profile.html',
   styleUrl: './organization-profile.css'
 })
 export class OrganizationProfile {
-
+icons = AppIcons;
 }
